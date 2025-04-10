86c43f5094519b0c361666c4bcae02d7147e7fb3
Ross Morgan <rossmmorgan@gmail.com>
2025-04-10 14:59:02 -0600

    test git ignore

---
+++ data/__pycache__/database.py
@@ -0,0 +1,6 @@
+import psycopg2
+
+conn = psycopg2.connect(database = 'spaceX_project_database',
+                        user = '',
+                        host ='localhost',
+                        password = '')
